d621e094f9f0575900ae680cfe5f459b8fb037ca
Wayne <waynehartman@gmail.com>
2016-08-16 20:27:34 -0500

    Initial Commit

---
+++ CameraPicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CameraPicker
+//
+//  Created by Wayne Hartman on 8/16/16.
+//  Copyright © 2016 Wayne Hartman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CameraPicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CameraPicker
+//
+//  Created by Wayne Hartman on 8/16/16.
+//  Copyright © 2016 Wayne Hartman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
